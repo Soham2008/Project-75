3a86cbb81b44293a28f3ddb0cca7ba81c89de2ab
Soham2008 <sohamswarup2008@gmail.com>
2021-06-24 22:26:23 +0530

    first commit

--- App.js
+++ App.js
@@ -1,15 +1,62 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
+import { createAppContainer, createSwitchNavigator } from 'react-navigation';
+import { createBottomTabNavigator } from 'react-navigation-tabs'
+import LoginScreen from './screens/LoginScreen';
+import WriteStoryScreen from './screens/WriteStoryScreen'
+import ReadStoryScreen from './screens/ReadStoryScreen'
+import { SafeAreaProvider } from 'react-native-safe-area-context';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+export default class App extends React.Component {
+  render() {
+    return (
+      <SafeAreaProvider>
+        <AppContainer />
+      </SafeAreaProvider>
+    );
+  }
 }
+
+const TabNavigator = createBottomTabNavigator({
+  WriteStory: WriteStoryScreen,
+  ReadStory: ReadStoryScreen
+},
+  {
+    defaultNavigationOptions: ({ navigation }) => ({
+      tabBarIcon: () => {
+        const routeName = navigation.state.routeName;
+        if (routeName === "WriteStory") {
+          return (
+            <SafeAreaProvider>
+              <Image
+                source={require("./assets/write.png")}
+                style={{ width: 40, height: 40 }}
+              />
+            </SafeAreaProvider>
+          )
+
+        }
+        else if (routeName === "ReadStory") {
+          return (
+            <SafeAreaProvider>
+              <Image
+                source={require("./assets/read.png")}
+                style={{ width: 40, height: 40 }}
+              />
+            </SafeAreaProvider>
+          )
+
+        }
+      }
+    })
+  })
+
+const SwitchNavigator = createSwitchNavigator({
+  LoginScreen: LoginScreen,
+  TabNavigator: TabNavigator
+})
+
+const AppContainer = createAppContainer(SwitchNavigator);
 
 const styles = StyleSheet.create({
   container: {
